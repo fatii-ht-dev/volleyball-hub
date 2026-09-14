a541cb74dad69f7b1fef5d1d585e8f6ede24b4bc
fatii_ht <fathb2000@gmail.com>
2026-09-14 14:59:29 +0330

    Adding player management.

--- volleyball_hub.py
+++ volleyball_hub.py
@@ -1,1 +1,43 @@
-print("Welcome to Volleyball Hub!🏐")
+players = []
+
+
+def add_player():
+    name = input("Player name: ")
+    position = input("Player position: ")
+    player = {
+        "name" : name,
+        "position" : position
+    }
+
+    players.append(player)
+
+def show_player():
+    if not players:
+        print("Oops! The player not found!")
+        return
+
+    print("\nPlayers: ")
+
+    for number, player in enumerate(players, start=1):
+        print(f"{number}.{player['name']} - {player['position']}")
+
+while True:
+    print("\n---Players Management---")
+    print("1. Add player")
+    print("2. Show player")
+    print("3. Exit")
+
+    choise = input("Coise an option: ")
+
+    if choise == "1":
+        add_player()
+
+    elif choise == "2":
+        show_player()
+
+    elif choise == "3":
+        print("See you later, Volleyball player!💕")
+        break
+
+    else:
+        print("Invalid choice.")
